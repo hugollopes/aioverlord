eb0fe4d801361754f0b1ec006c5c38e74d2fa22e
asd <asd>
2017-09-24 19:05:07 +0200

    yay

---
+++ hello world.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from flask import Response
+from flask import jsonify
+
+app = Flask(__name__, static_url_path='')
+
+
+
+class User(object):
+    name = ""
+    age = 0
+
+    # The class "constructor" - It's actually an initializer
+    def __init__(self, name, age, ):
+        self.name = name
+        self.age = age
+
+
+def make_user(name, age):
+    student = User(name, age, )
+    return student
+
+@app.route("/")
+def hello():
+    user = make_user("hugo",14)
+    resp = jsonify(
+        name=user.name,
+        age=user.age,
+        )
+    #resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+
+@app.route("/attack")
+def hellow():
+    return "Hello World!2222"
+
+@app.route('/<path:path>')
+def root(path):
+    return app.send_static_file(path)
+    #return "here"
+
+if __name__ == "__main__":
+    app.run()
+
+
+
+
---
+++ static/code.js
@@ -0,0 +1,11 @@
+const vm = new Vue({
+  el: '#app',
+  data: {
+    results: []
+  },
+  mounted() {
+    axios.get("http://127.0.0.1:5000/")
+    .then(response => {this.results = response.data})
+  }
+});
+
