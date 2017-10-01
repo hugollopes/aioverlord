9677ca0a8d3856151d8fae93bcc1fac5a015ff1e
hugollopes <asd>
2017-10-01 15:27:17 +0200

    update

---
+++ src/components/user.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <div class="row">
+    <div class="col-6 col-sm-3">Cash</div>
+    <div class="col-6 col-sm-3">{{credits}}</div>
+    <div class="col-6 col-sm-3">Neurons</div>
+    <div class="col-6 col-sm-3">4757663</div>
+  </div>
+
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'user',
+  data () {
+    return {
+      name: "",
+      credits: 1
+    }
+  },
+  created: function(){
+    axios.get("http://127.0.0.1:50000/getuser")
+    .then(response => {this.name = response.data.name;
+      this.credits = response.data.cash})
+    }
+  }
+  </script>
