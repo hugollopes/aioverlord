6172e91283df423c8cd9955e6ea0a220dc498dad
hugollopes <hugo.l.lopes@gmail.com>
2017-12-10 13:05:59 +0100

    update

--- docker/startupdev.sh
+++ docker/startupdev.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 #docker build
-docker build -t flask .
-
-#docker run
-docker run -p 5000:5000 flask
-docker run --name mongodb -d -p 27017:27017 mongo:3.5
-
-docker-compose -f e2e-test.yml up
-
+docker-compose -f dev.yml build
 docker-compose -f dev.yml up
