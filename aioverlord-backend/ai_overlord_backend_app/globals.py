a678689b76e57be8e946f9a9d9767f07d5254007
hugollopes <hugo.l.lopes@gmail.com>
2018-02-20 23:10:28 +0100

    update

--- aioverlord-backend/ai_overlord_backend_app/globals.py
+++ aioverlord-backend/ai_overlord_backend_app/globals.py
@@ -1,3 +1,8 @@
-TOPOLOGIES = [{"id": 1, "name": "2 hidden layers", "cost": 10},
-              {"id": 2, "name": "3 hidden layers", "cost": 100}]
-COST_PER_NEURON = 100
+import json
+
+with open("./static/referenceData/referenceData.json", encoding='utf-8') as data_file:
+    data = json.loads(data_file.read())
+
+TOPOLOGIES = data["topologies"]
+
+COST_PER_NEURON = data["costPerNeuron"]
