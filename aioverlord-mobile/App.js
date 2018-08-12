abf5942976f7e97af2866e41c609691f95d1f4f6
hugollopes <hugo.l.lopes@gmail.com>
2018-08-12 11:22:35 +0200

    u

--- aioverlord-mobile/App.js
+++ aioverlord-mobile/App.js
@@ -21,7 +21,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!  yeahhh</Text>
+        <Text style={styles.welcome}>Welcome to React Native!  yeahhh3</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
